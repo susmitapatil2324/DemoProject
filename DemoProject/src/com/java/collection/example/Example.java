47768a2b6c01d647b3795ecbfc2038f86ce80e50
Sushmita <patilsusmita2331@gmail.com>
2022-11-13 20:07:18 +0530

    First commit

---
+++ DemoProject/src/com/java/collection/example/Example.java
@@ -0,0 +1,19 @@
+package com.java.collection.example;
+
+import java.util.ArrayList;
+
+public class Example {
+
+	public static void main(String[] args) {
+	ArrayList arr = new ArrayList();
+	arr.add(10);
+	arr.add(20);
+	arr.add(30);
+	arr.add(40);
+	arr.add(50);
+	
+	System.out.println(arr);
+	
+	}
+
+}
