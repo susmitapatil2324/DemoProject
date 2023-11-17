ec5cefe7386cab996aae754e7c981146e8cb1709
Susmita Patil <58322005+susmitapatil2324@users.noreply.github.com>
2023-11-17 12:16:20 +0530

    Time: 1 ms (100.00%) | Memory: 45.7 MB (7.02%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,22 +1,25 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        if (nums.length == 1) {
-            return;
+        if(nums.length==0)
+        {
+            return ;
         }
 
-        int nonZeroIndex = 0;
+        int nonZeroCount=0;
 
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != 0) {
-                nums[nonZeroIndex] = nums[i];
-                nonZeroIndex++;
-            }
+        for(int i=0;i<nums.length;i++)
+        {
+           if(nums[i]!=0)
+           {
+             nums[nonZeroCount] =nums[i];
+             nonZeroCount++;
+           }
         }
 
-       
-         while(nonZeroIndex<nums.length){
-            nums[nonZeroIndex] = 0;
-            nonZeroIndex++;
+        while(nonZeroCount<nums.length)
+        {
+           nums[nonZeroCount]=0;
+           nonZeroCount++;;
         }
     }
 }
