f43702ad84adb384df296db03c75fbd4ae6054f0
Susmita Patil <58322005+susmitapatil2324@users.noreply.github.com>
2023-11-17 12:37:42 +0530

    Time: 2 ms (99.45%) | Memory: 43.9 MB (6.35%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,28 +1,39 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int len1 = nums1.length;
+        //Taking length of two array
+        int len1 = nums1.length; 
         int len2 = nums2.length;
-        HashMap<Integer, Integer> hm = new HashMap<>();
-        for (int i = 0; i < len2; i++) {
-            hm.put(nums2[i], i);
-        }   
 
-        int[] res = new int[len1];
+        //Created HashMap
+        Map<Integer,Integer> map = new HashMap<>();
+
+        //Created result array
+        int result[] = new int[len1];
+
         for(int i=0;i<len1;i++)
-        res[i]=-1;
-
-        for (int i = 0; i < len1; i++) {
-            int num = nums1[i];
-            int index = hm.get(num);
-           
-            for (int j = index + 1; j < len2; j++) {
-                if (num < nums2[j]) {
-                    res[i] = nums2[j];
-                  
-                    break;
-                }
-            }
+        {
+           result[i] = -1;
         }
-        return res;
+
+        for(int i=0;i<len2;i++)
+        {
+            map.put(nums2[i],i);
+        }
+
+        for(int i=0;i<len1;i++)
+        {
+           int no = nums1[i];
+           int index = map.get(no);
+
+           for(int j =index+1;j<len2;j++)
+           {
+              if(no<nums2[j])
+              {
+                result[i] =nums2[j];
+                break;
+              }
+           }
+        }
+       return result;
     }
 }
