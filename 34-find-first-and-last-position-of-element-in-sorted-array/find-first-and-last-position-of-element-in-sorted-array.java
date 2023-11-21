c88f2c785b83d7941bad3a8522ab5b781991edc0
Susmita Patil <58322005+susmitapatil2324@users.noreply.github.com>
2023-11-21 21:00:28 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (87.84%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,42 +1,58 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int[] res = new int[2];
-        res[0] = findStartPos(nums, target);
-        res[1] = findEndPos(nums, target);
-        return res;
-    }
+        int result[] = new int[2];
+        result[0] = findFirstIndex(nums,target);
+        result[1] = findLastIndex(nums,target);
+        return result;
+      }
 
-    private int findStartPos(int[] nums, int target) {
-        int left = 0, right = nums.length - 1, ans = -1;
-        while (left <= right) {
-            int midPos = left + (right - left) / 2;
+   private int findFirstIndex(int[] nums, int target)
+   {
+     int left = 0;
+     int right = nums.length-1;
+     int ans =-1;
 
-            if (target == nums[midPos]) {
-                ans = midPos;
-                right = midPos - 1;
-            } else if (target > nums[midPos]) {
-                left = midPos + 1;
-            } else {
-                right = midPos - 1;
-            }
-        }
-        return ans;
-    }
+     while(left<=right)
+     {
+       int mid = left+(right-left)/2;
 
-    private int findEndPos(int[] nums, int target) {
-        int left = 0, right = nums.length - 1, ans = -1;
-        while (left <= right) {
-            int midPos = left + (right - left) / 2;
+       if(target==nums[mid])
+       {
+          ans = mid;
+          right= mid-1;
+       }else if(target<nums[mid])
+       {
+         right = mid-1;
+       }else
+       {
+         left = mid+1;
+       }
+     }
+     return ans;
+   }   
+   
+   private int findLastIndex(int[] nums, int target)
+   {
+       int left = 0;
+       int right = nums.length-1;
+       int ans =-1;
 
-            if (target == nums[midPos]) {
-                ans = midPos;
-                left = midPos + 1;
-            } else if (target > nums[midPos]) {
-                left = midPos + 1;
-            } else {
-                right = midPos - 1;
-            }
-        }
-        return ans;
-    }
+       while(left<=right)
+       {
+         int mid = left+(right-left)/2;
+
+         if(target==nums[mid])
+         {
+          ans =mid;
+          left =mid+1;
+         }else if(nums[mid]<target)
+         {
+            left =mid+1;
+         }else
+         {
+           right = mid-1;
+         }
+       }
+       return ans;
+   }
 }
