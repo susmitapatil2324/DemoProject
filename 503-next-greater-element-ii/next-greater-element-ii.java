50587011f6d01d013566adb76de86420a144bc82
Susmita Patil <58322005+susmitapatil2324@users.noreply.github.com>
2023-11-24 13:00:57 +0530

    Time: 13 ms (73.87%) | Memory: 44.5 MB (84.19%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.java
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -1,25 +1,25 @@
 class Solution {
+    
     public int[] nextGreaterElements(int[] nums) {
-        
-        Stack<Integer> stack=new Stack<>();
+        Stack<Integer> stack = new Stack<>();
 
-        // Push all the element in the stack
-    for(int i=nums.length-1;i>=0;i--){
-            stack.push(nums[i]);
-     }
-        
-        //Check one by one in stack any greater element is there
-    for(int i=nums.length-1;i>=0;i--){
-        int number=nums[i];
+        for(int i=nums.length-1;i>=0;i--)
+        {
+          stack.push(nums[i]);
+        }
 
-        while(stack.size()!=0 && stack.peek()<=nums[i]){
-                    stack.pop();
+        for(int i= nums.length-1;i>=0;i--)
+        {
+            int number = nums[i];
+           while(stack.size()!=0 && stack.peek()<=nums[i])
+           {
+               stack.pop();
+           }
+
+           nums[i] = stack.empty()?-1:stack.peek();
+           stack.push(number);
         }
-           
-            nums[i]=stack.empty()?-1:stack.peek();
-            stack.push(number);
-        }
-        
+
         return nums;
     }
 }
