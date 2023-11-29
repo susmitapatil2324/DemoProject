5dacaac4b72d5d8ca86c4cc52771707df37455cd
Susmita Patil <58322005+susmitapatil2324@users.noreply.github.com>
2023-11-29 10:14:30 +0530

    Time: 1 ms (98.29%) | Memory: 44 MB (20.92%) - LeetSync

--- 1168-duplicate-zeros/duplicate-zeros.java
+++ 1168-duplicate-zeros/duplicate-zeros.java
@@ -1,20 +1,24 @@
 class Solution {
     public void duplicateZeros(int[] arr) {
-        int length = arr.length;
-        int[] res = new int[length];
-        int j = 0;
-        
-        for (int i = 0; i < length && j < length; i++){
-            if (arr[i] != 0){
-                res[j] = arr[i];
-            } else {
-                j++;
-            }
+       int n = arr.length;
+       int result[] = new int[arr.length];
+       int j=0;
+
+       for(int i=0;i<n && j<n;i++)
+       {
+          if(arr[i]!=0)
+          {
+            result[j] =arr[i];
+          }else
+          {
             j++;
-        }
-        
-        for (int i = 0; i < length; i++){
-            arr[i] = res[i];
-        }
+          }
+          j++;
+       }
+
+       for(int i=0;i<n;i++)
+       {
+         arr[i] =result[i];
+       }
     }
 }
