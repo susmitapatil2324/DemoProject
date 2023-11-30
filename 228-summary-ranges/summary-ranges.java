110e59ec158bd642540d6736c7eb32fe52ea7df0
Susmita Patil <58322005+susmitapatil2324@users.noreply.github.com>
2023-11-30 21:08:24 +0530

    Time: 6 ms (53.73%) | Memory: 40.4 MB (86.41%) - LeetSync

--- 228-summary-ranges/summary-ranges.java
+++ 228-summary-ranges/summary-ranges.java
@@ -1,24 +1,28 @@
 class Solution {
-    
+
     public List<String> summaryRanges(int[] nums) {
-        List<String> list = new ArrayList<>();
+       List<String> list = new ArrayList<>();
 
-        for(int i=0;i<nums.length;i++)
-        {
-            int start= nums[i];
+       for(int i=0;i<nums.length;i++)
+       {
+           int start = nums[i];
 
-            while(i<nums.length-1 && nums[i+1]-nums[i]==1)
-            {
-               i++;
-            }
-           String result = getRanges(start,nums[i]);
-           list.add(result);
-        }
-        return list;
+           while(i<nums.length-1 && nums[i]+1 ==nums[i+1])
+           {
+              i++;
+           }
+
+           if(start!=nums[i])
+           {
+            String result1 = String.valueOf(start)+"->"+String.valueOf(nums[i]);
+            list.add(result1);
+           }else
+           {
+              String result2 = String.valueOf(start);
+              list.add(result2);
+           }
+       }
+      return list;
     }
 
-    public String getRanges(int to ,int from)
-    {
-        return (to==from)? String.valueOf(to):String.valueOf(to)+"->"+String.valueOf(from);
-    }
 }
