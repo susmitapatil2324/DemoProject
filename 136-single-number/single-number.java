4d03259dbd4b6f103a83533220da1e8682cbb47c
Susmita Patil <58322005+susmitapatil2324@users.noreply.github.com>
2023-12-02 21:50:44 +0530

    Time: 1 ms (99.93%) | Memory: 44.8 MB (28.16%) - LeetSync

---
+++ 136-single-number/single-number.java
@@ -0,0 +1,13 @@
+class Solution {
+
+    public int singleNumber(int[] nums) {
+      int xor =0;
+      int result =0;
+
+      for(int i=0;i<nums.length;i++)
+      {
+          xor^= nums[i];
+      }
+      return xor;
+    }
+}
