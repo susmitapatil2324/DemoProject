9a748ed4ff3f8f1f702bba5a8b510f211ecbb486
Susmita Patil <58322005+susmitapatil2324@users.noreply.github.com>
2023-12-10 01:27:59 +0530

    Time: 1 ms (81.43%) | Memory: 40.6 MB (5.36%) - LeetSync

--- 202-happy-number/happy-number.java
+++ 202-happy-number/happy-number.java
@@ -2,28 +2,25 @@ class Solution {
 
      // Main function to check if a number is happy
     public boolean isHappy(int n) {
-       // Set to keep track of seen numbers
-        HashSet<Integer> seenNumbers = new HashSet<>();
+     Set<Integer> set = new HashSet<>();
 
-        // Continue the process until n becomes 1 or we encounter a cycle
-        while (n != 1 && !seenNumbers.contains(n)) {
-            seenNumbers.add(n);
-            n = getSquareSum(n);
-        }
-
-        // If n is 1, it's a happy number; otherwise, it's not
-        return n == 1;
+     while(n!=1 && !set.contains(n))
+     {
+        set.add(n);
+        n =getSquareNumber(n);
+     }
+    return n==1;
     }
 
-    private static int getSquareSum(int n) {
-        int result = 0;
-        while (n > 0) {
-            int digit = n % 10;
-            result += digit * digit;
-            n /= 10;
+    public static int getSquareNumber(int n)
+    {
+         int result=0;
+        while(n>0)
+        {
+          int no = n%10;
+          result += no*no ;
+          n/=10;
         }
-        return result;
+      return result;  
     }
-
-   
 }
