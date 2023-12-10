b2235deab26f25b6c08947dfac8ec384d3a199bc
Susmita Patil <58322005+susmitapatil2324@users.noreply.github.com>
2023-12-10 19:58:57 +0530

    Time: 2 ms (64.44%) | Memory: 52.5 MB (56.54%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -4,29 +4,29 @@ class Solution {
       int n = nums.length;  
       int result[] = new int[n];  
 
-      int leftSum[] = new int[n];
-      int rightSum[] = new int[n];
-
-      leftSum[0] = 1;
-
-      for(int i=1;i<n;i++)
-      {
-        leftSum[i] = leftSum[i-1]*nums[i-1];
-      }
-
-      rightSum[n-1] =1;
-
-      for(int i =n-2;i>=0;i--)
-      {
-         rightSum[i] = rightSum[i+1] * nums[i+1]; 
-      }
+      //First Calculate PrefixProduct
+      int prefixProduct=1;
 
       for(int i=0;i<n;i++)
       {
-          result[i] = leftSum[i]*rightSum[i];
+          if(i>0)
+          {
+            prefixProduct*=nums[i-1];
+          }
+             result[i] = prefixProduct;
       }
+      
+      //Second Calculate SufixProduct
+      int sufixProduct=1;
 
-
+      for(int i=n-1;i>=0;i--)
+      {
+         if(i<n-1)
+         {
+           sufixProduct *= nums[i+1]; 
+         }
+         result[i] *= sufixProduct;
+      }
       return result;
     }
 }
