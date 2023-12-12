83b58ff4f0cacbbdb4a99199b26f618e758a3a27
Susmita Patil <58322005+susmitapatil2324@users.noreply.github.com>
2023-12-13 00:20:30 +0530

    Time: 141 ms (5.02%) | Memory: 44.7 MB (12.60%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -1,21 +1,28 @@
 class Solution {
 
     public int missingNumber(int[] nums) {
-     int n = nums.length ; // Number of elements including the missing one
+     int n = nums.length;
 
-        // XOR all numbers from 1 to n
-        int xorAll = 0;
-        for (int i = 1; i <= n; i++) {
-            xorAll ^= i;
-        }
+     int xorAll =0;
 
-        // XOR all elements in the array
-        int xorArray = 0;
-        for (int num : nums) {
+     for(int i=1;i<=n;i++)
+     {
+         xorAll^= i;
+     }
+
+     System.out.println("xorAll is :"+xorAll);
+
+     int xorArray= 0;
+
+     for(int num : nums) {
+         System.out.println("nums is:"+num);
             xorArray ^= num;
-        }
+     }
 
-        // XOR the results to find the missing number
-        return xorAll ^ xorArray;
+    System.out.println("xorArray is :"+xorArray);
+
+    System.out.println("Sub is :"+(xorAll^xorArray));
+
+    return xorAll^xorArray;
     }
 }
