bebb53f745c5759757d1f8066e25511cd80f0eeb
Susmita Patil <58322005+susmitapatil2324@users.noreply.github.com>
2023-12-13 00:52:59 +0530

    Time: 0 ms (100.00%) | Memory: 39.9 MB (24.60%) - LeetSync

--- 231-power-of-two/power-of-two.java
+++ 231-power-of-two/power-of-two.java
@@ -1,12 +1,21 @@
 class Solution {
     
     public boolean isPowerOfTwo(int n) {
+
      //Binary Representation of 2 is 1 Bit by using this formula 8421
+     //If any number less than zero it is not power of two hence return false
       if(n<=0)
       {
           return false;
       }  
 
       return ((n &(n-1))==0);
+
+      /* for n=8    1000  (n)
+                    & 0111  (n - 1)
+                   -----------
+                    0000
+         In this case it return true as 8 is power of 2             
+        */
     }
 }
