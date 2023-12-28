496850d194474bcc2284ee07f2c02746b46db7e6
Susmita Patil <58322005+susmitapatil2324@users.noreply.github.com>
2023-12-28 20:10:34 +0530

    Time: 36 ms (12.10%) | Memory: 58 MB (5.83%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.java
+++ 219-contains-duplicate-ii/contains-duplicate-ii.java
@@ -5,8 +5,11 @@ class Solution {
         Map<Integer, Integer> hashMap = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             int integer = nums[i];
+            
             if (hashMap.containsKey(integer) && i - hashMap.get(integer) <= k)
+            {
                 return true;
+            }
             hashMap.put(integer, i);
         }
         return false;
