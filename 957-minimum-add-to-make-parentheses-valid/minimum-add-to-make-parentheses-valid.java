88c91e216e18b7ca5f929b646322d5a737e49603
Susmita Patil <58322005+susmitapatil2324@users.noreply.github.com>
2024-01-07 19:47:28 +0530

    Time: 2 ms (34.66%) | Memory: 41.4 MB (12.97%) - LeetSync

--- 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
@@ -1,18 +1,30 @@
 class Solution {
+
     public int minAddToMakeValid(String s) {
-        int openCount = 0;  // Number of open parentheses needed
-        int closeCount = 0; // Number of close parentheses needed
+      Stack<Character> stack = new Stack<>();
 
-        for (char c : s.toCharArray()) {
-            if (c == '(') {
-                openCount++;
-            } else if (c == ')' && openCount > 0) {
-                openCount--;
-            } else {
-                closeCount++;
+      for(int i=0;i<s.length();i++)
+      {
+         if(s.charAt(i)==')')
+         {
+            /*If opposite bracket is there and stack is not empty   pop it  */
+            if(!stack.isEmpty() && stack.peek()=='(' )
+            {
+               stack.pop();
+            }else
+            {
+              /* If stack is empty or top element is not equal to '('
+               */  
+              stack.push(s.charAt(i));
             }
-        }
 
-        return openCount + closeCount;
+         }else
+         {
+          // If we have opening bracket push it to stack   
+            stack.push(s.charAt(i));
+         }
+      }  
+      //return the size of stack we will have bracket which are not matching 
+      return stack.size();
     }
 }
