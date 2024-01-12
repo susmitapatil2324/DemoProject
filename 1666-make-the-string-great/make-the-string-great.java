42e4ce773e7ff4db309400a07a8d031840c8a23c
Susmita Patil <58322005+susmitapatil2324@users.noreply.github.com>
2024-01-12 11:01:52 +0530

    Time: 3 ms (64.62%) | Memory: 42.4 MB (26.82%) - LeetSync

--- 1666-make-the-string-great/make-the-string-great.java
+++ 1666-make-the-string-great/make-the-string-great.java
@@ -3,9 +3,9 @@ class Solution {
         int n = s.length();
         Stack<Character> st = new Stack<>();
         String ans = "";
-        st.push(s.charAt(0));
+      //  st.push(s.charAt(0));
         char[] arr = s.toCharArray();
-        for(int i=1; i<n ;i++){
+        for(int i=0; i<n ;i++){
             if(!st.isEmpty() && (st.peek()-arr[i]==32 || st.peek()-arr[i]==-32)){
                 st.pop();
             }else{
