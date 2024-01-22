7a264870ea0f95683310c99a017ae81a5966e2c6
Susmita Patil <58322005+susmitapatil2324@users.noreply.github.com>
2024-01-22 13:39:26 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (5.79%) - LeetSync

--- 92-reverse-linked-list-ii/reverse-linked-list-ii.java
+++ 92-reverse-linked-list-ii/reverse-linked-list-ii.java
@@ -9,31 +9,32 @@
  * }
  */
 class Solution {
+
     public ListNode reverseBetween(ListNode head, int left, int right) {
+       if(head.next==null || head==null)
+       {
+         return head;
+       }
 
-      if(head.next==null || head==null)
-      {
-          return head;
-      }
-      ListNode dummy = new ListNode(0);
-      dummy.next = head;
-      
-      ListNode nodeBeforeSubList = dummy;
+       //Create Dummy node so that if head is left we don't loose left
+       ListNode dummy = new ListNode(0);
+       dummy.next = head;
+       ListNode nodeBeforeSubList = dummy;
 
-      for(int i=0;i<left-1;i++)
-      {
-        nodeBeforeSubList = nodeBeforeSubList.next;
-      }
-      ListNode workingpt = nodeBeforeSubList.next;
-
-      for(int i=0;i<right-left;i++)
-      {
-         ListNode nodeToBeExacted = workingpt.next;
-         workingpt.next = nodeToBeExacted.next;
-         nodeToBeExacted.next = nodeBeforeSubList.next;
-         nodeBeforeSubList.next = nodeToBeExacted;
-      }
-
-     return dummy.next;
+       for(int i=0;i<left-1;i++)
+       {
+           nodeBeforeSubList = nodeBeforeSubList.next;
+       } 
+       
+       ListNode workingNode = nodeBeforeSubList.next;
+        
+       for(int i=0;i<right-left;i++)
+       {
+        ListNode nodeTobeExtracted = workingNode.next;
+        workingNode.next = nodeTobeExtracted.next;
+        nodeTobeExtracted.next = nodeBeforeSubList.next;
+        nodeBeforeSubList.next = nodeTobeExtracted;     
+       }
+       return dummy.next;
     }
 }
