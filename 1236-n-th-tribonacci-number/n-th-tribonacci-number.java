f4af2d3a36efe5f762599bf407bb1a934dbd8087
Susmita Patil <58322005+susmitapatil2324@users.noreply.github.com>
2024-02-17 12:58:32 +0530

    Time: 0 ms (100.00%) | Memory: 40.1 MB (81.81%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
@@ -8,7 +8,7 @@ class Solution {
 
         for(int i=3;i<=n;i++)
         {
-            dp[i] = dp[i-3]+dp[i-2]+dp[i-1];
+            dp[i] = dp[i-1]+dp[i-2]+dp[i-3];
         }
 
         return dp[n];
