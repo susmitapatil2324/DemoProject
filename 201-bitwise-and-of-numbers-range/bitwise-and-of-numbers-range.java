59f4e2f88a78d77f3fa5aa894801a39b6aea3591
Susmita Patil <58322005+susmitapatil2324@users.noreply.github.com>
2024-02-29 00:21:50 +0530

    Time: 3 ms (100.00%) | Memory: 43.7 MB (58.46%) - LeetSync

--- 201-bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.java
+++ 201-bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.java
@@ -1,12 +1,12 @@
 class Solution {
-    
+
     public int rangeBitwiseAnd(int left, int right) {
-        int cnt = 0;
-        while (left != right) {
-            left >>= 1;
-            right >>= 1;
-            cnt++;
-        }
-        return (left << cnt);
+
+     while(right>left)
+     {
+       right = (right & right-1);
+     }
+     return right;
+
     }
 }
