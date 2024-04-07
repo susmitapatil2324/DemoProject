7ba3d868a1b1862d1ddb4ef044855f08f4e2e4ef
Susmita Patil <58322005+susmitapatil2324@users.noreply.github.com>
2024-04-07 23:00:25 +0530

    Time: 43 ms (10.52%) | Memory: 45 MB (5.02%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
@@ -14,12 +14,18 @@
  * }
  */
 class Solution {
+
     public int maxDepth(TreeNode root) {
-        if(root == null){
-            return 0;
-        }
-        int lh = maxDepth(root.left);
-        int rh = maxDepth(root.right);
-        return  Math.max(lh,rh)+1;
+       if(root==null)
+       {
+        return 0;
+       }
+
+       int left  = maxDepth(root.left);
+       System.out.println("Left is :"+left);
+       int right = maxDepth(root.right);
+       System.out.println("Right is :"+right);
+
+       return Math.max(left,right)+1;
     }
 }
