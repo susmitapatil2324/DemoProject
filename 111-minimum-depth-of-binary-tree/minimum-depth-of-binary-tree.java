2f48cb4bc28178301a93d85af4494ac8ee542362
Susmita Patil <58322005+susmitapatil2324@users.noreply.github.com>
2024-10-05 15:51:50 +0530

    Time: 4 ms (78.45%) | Memory: 63.1 MB (55.31%) - LeetSync

--- 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.java
+++ 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.java
@@ -1,25 +1,20 @@
 class Solution {
     
     public int minDepth(TreeNode root) {
+      
         if(root==null){
             return 0;
-        }
+        }  
 
-        int leftDepth = minDepth(root.left);
+        int leftDepth  = minDepth(root.left);
         int rightDepth = minDepth(root.right);
 
-        if(root.left==null && root.right==null){
-            return 1;
-        }
-
-        if(root.left==null){
-            return 1+rightDepth;
-        }
-
-        if(root.right==null){
-            return 1+leftDepth;
-        }
-
-        return Math.min(leftDepth,rightDepth)+1;
+        //Skewed Tree
+        if(Math.min(leftDepth,rightDepth)==0)
+        {
+            return 1+Math.max(leftDepth,rightDepth);
+        }else
+        //Non-Skewed Tree
+         return 1+Math.min(leftDepth,rightDepth);
    }
 }
