35ed7a42af8394e843188ca4d16e54a86abd7fa1
Susmita Patil <58322005+susmitapatil2324@users.noreply.github.com>
2025-02-17 23:14:21 +0530

    Time: 6 ms (70.62%) | Memory: 44.8 MB (54.69%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,28 +1,21 @@
 class Solution {
     
     public int lengthOfLongestSubstring(String s) {
-        /* We are using two pointer here. if there is unique char will move our 
-           right pointer. if there is repeated char present in that case will move our
-           left pointer.
-
-           if you are confuse check this video on youtube:
-           https://www.youtube.com/watch?v=jHj13UHURr8
-        */
        int left =0;
        int maxLength =0;
+       Set<Character> set = new HashSet<>();
 
-       Set<Character> set = new HashSet<>(); 
-
-       for(int right=0;right<s.length();right++){
-
-           while(set.contains(s.charAt(right)))
-           {
-             set.remove(s.charAt(left));
-             left++;
-           }
-          set.add(s.charAt(right));
-          maxLength = Math.max(maxLength ,(right-left+1));
+       for(int right =0;right<s.length();right++){
+         
+         while(set.contains(s.charAt(right))){
+           set.remove(s.charAt(left));
+           left++;
+         }
+         set.add(s.charAt(right));
+         
+         maxLength = Math.max(maxLength,right-left+1);
        }
-     return maxLength;
+
+       return maxLength;
     }
 }
